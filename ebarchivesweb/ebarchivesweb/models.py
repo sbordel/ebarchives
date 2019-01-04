e21a4fefafb03f855afe9e1ca39d2206bcbda759
Tiago Bortoletto Vaz <tiago@debian.org>
2019-01-04 15:40:54 -0500

    New view parameters.

--- ebarchivesweb/ebarchivesweb/models.py
+++ ebarchivesweb/ebarchivesweb/models.py
@@ -42,15 +42,27 @@ class Event(models.Model):
     type = models.ForeignKey('EventType', on_delete=models.SET_NULL, null=True)
     year = models.PositiveSmallIntegerField(blank=True, null=True)
 
+    def __str__(self):
+        return "%s" % (self.title)
+
 class Artist(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     email = models.EmailField()
 
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
 class EventType(models.Model):
     type = models.CharField(choices=EVENT_CHOICES, max_length=200)
     recurring_event = models.ForeignKey('RecurringEvent', on_delete=models.SET_NULL, null=True, blank=True)
 
+    def __str__(self):
+        return "%s" % (self.type)
+
 class RecurringEvent(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return "%s" % (self.title)
--- ebarchivesweb/ebarchivesweb/views.py
+++ ebarchivesweb/ebarchivesweb/views.py
@@ -9,6 +9,9 @@ def index(request):
 def exhibit(request, year=None, letter=None):
    event_type = 'exhibit'
    event_title = 'exhibits'
+   year_range = range(2008,2020)
+   alpha_range1 = [chr(i) for i in range(ord('A'),ord('M')+1)]
+   alpha_range2 = [chr(i) for i in range(ord('N'),ord('Z')+1)]
    events = Event.objects.filter(type__type__exact=event_type)
    if year is not None:
        events = events.filter(year=year)
@@ -18,12 +21,19 @@ def exhibit(request, year=None, letter=None):
    context = {
         'events': events,
         'event_title': event_title,
+        'event_type': event_type,
+        'year_range': year_range,
+        'alpha_range1': alpha_range1,
+        'alpha_range2': alpha_range2,
     }
    return render(request, "general_event.html", context=context)
 
 def festival(request, year=None, letter=None):
    event_type = 'festival'
    event_title = 'festivals'
+   year_range = range(2008,2020)
+   alpha_range1 = [chr(i) for i in range(ord('A'),ord('M')+1)]
+   alpha_range2 = [chr(i) for i in range(ord('N'),ord('Z')+1)]
    events = Event.objects.filter(type__type__exact=event_type)
    if year is not None:
        events = events.filter(year=year)
@@ -33,12 +43,19 @@ def festival(request, year=None, letter=None):
    context = {
         'events': events,
         'event_title': event_title,
+        'event_type': event_type,
+        'year_range': year_range,
+        'alpha_range1': alpha_range1,
+        'alpha_range2': alpha_range2,
     }
    return render(request, "general_event.html", context=context)
 
 def workshop(request, year=None, letter=None):
    event_type = 'workshop'
    event_title = 'workshops'
+   year_range = range(2008,2020)
+   alpha_range1 = [chr(i) for i in range(ord('A'),ord('M')+1)]
+   alpha_range2 = [chr(i) for i in range(ord('N'),ord('Z')+1)]
    events = Event.objects.filter(type__type__exact=event_type)
    if year is not None:
        events = events.filter(year=year)
@@ -48,12 +65,19 @@ def workshop(request, year=None, letter=None):
    context = {
         'events': events,
         'event_title': event_title,
+        'event_type': event_type,
+        'year_range': year_range,
+        'alpha_range1': alpha_range1,
+        'alpha_range2': alpha_range2,
     }
    return render(request, "general_event.html", context=context)
 
 def talk(request, year=None, letter=None):
    event_type = 'talk'
    event_title = 'talks'
+   year_range = range(2008,2020)
+   alpha_range1 = [chr(i) for i in range(ord('A'),ord('M')+1)]
+   alpha_range2 = [chr(i) for i in range(ord('N'),ord('Z')+1)]
    events = Event.objects.filter(type__type__exact=event_type)
    if year is not None:
        events = events.filter(year=year)
@@ -63,12 +87,19 @@ def talk(request, year=None, letter=None):
    context = {
         'events': events,
         'event_title': event_title,
+        'event_type': event_type,
+        'year_range': year_range,
+        'alpha_range1': alpha_range1,
+        'alpha_range2': alpha_range2,
     }
    return render(request, "general_event.html", context=context)
 
 def event(request, year=None, letter=None):
    event_type = 'event'
    event_title = 'events'
+   year_range = range(2008,2020)
+   alpha_range1 = [chr(i) for i in range(ord('A'),ord('M')+1)]
+   alpha_range2 = [chr(i) for i in range(ord('N'),ord('Z')+1)]
    events = Event.objects.filter(type__type__exact=event_type)
    if year is not None:
        events = events.filter(year=year)
@@ -78,12 +109,19 @@ def event(request, year=None, letter=None):
    context = {
         'events': events,
         'event_title': event_title,
+        'event_type': event_type,
+        'year_range': year_range,
+        'alpha_range1': alpha_range1,
+        'alpha_range2': alpha_range2,
     }
    return render(request, "general_event.html", context=context)
 
 def residency(request, year=None, letter=None):
    event_type = 'residency'
    event_title = 'residencies'
+   year_range = range(2008,2020)
+   alpha_range1 = [chr(i) for i in range(ord('A'),ord('M')+1)]
+   alpha_range2 = [chr(i) for i in range(ord('N'),ord('Z')+1)]
    events = Event.objects.filter(type__type__exact=event_type)
    if year is not None:
        events = events.filter(year=year)
@@ -93,12 +131,19 @@ def residency(request, year=None, letter=None):
    context = {
         'events': events,
         'event_title': event_title,
+        'event_type': event_type,
+        'year_range': year_range,
+        'alpha_range1': alpha_range1,
+        'alpha_range2': alpha_range2,
     }
    return render(request, "general_event.html", context=context)
 
 def misc(request, year=None, letter=None):
    event_type = 'misc'
    event_title = 'misc'
+   year_range = range(2008,2020)
+   alpha_range1 = [chr(i) for i in range(ord('A'),ord('M')+1)]
+   alpha_range2 = [chr(i) for i in range(ord('N'),ord('Z')+1)]
    events = Event.objects.filter(type__type__exact=event_type)
    if year is not None:
        events = events.filter(year=year)
@@ -108,6 +153,10 @@ def misc(request, year=None, letter=None):
    context = {
         'events': events,
         'event_title': event_title,
+        'event_type': event_type,
+        'year_range': year_range,
+        'alpha_range1': alpha_range1,
+        'alpha_range2': alpha_range2,
     }
    return render(request, "general_event.html", context=context)
 
