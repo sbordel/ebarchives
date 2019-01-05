d17eaae1ca81f9a8d8c81e9288a2ac3cb208ddc4
Tiago Bortoletto Vaz <tiago@debian.org>
2019-01-05 15:58:27 -0500

    Add Media class.

--- ebarchivesweb/ebarchivesweb/admin.py
+++ ebarchivesweb/ebarchivesweb/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Event, EventType, RecurringEvent, Artist
+from .models import Event, EventType, RecurringEvent, Artist, Media
 
 admin.site.register(Event)
 admin.site.register(EventType)
 admin.site.register(RecurringEvent)
 admin.site.register(Artist)
+admin.site.register(Media)
--- ebarchivesweb/ebarchivesweb/models.py
+++ ebarchivesweb/ebarchivesweb/models.py
@@ -35,15 +35,44 @@ EVENT_CHOICES = (
     )
 )
 
+MEDIA_CHOICES = (
+    (
+        "image",
+        "Image",
+    ),
+    (
+        "audio",
+        "Audio",
+    ),
+    (
+        "video",
+        "Video",
+    ),
+    (
+        "other",
+        "Other",
+    )
+)
+
 class Event(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     artist = models.ManyToManyField('Artist', blank=True)
+    media = models.ManyToManyField('Media', blank=True)
     type = models.ForeignKey('EventType', on_delete=models.SET_NULL, null=True)
     year = models.PositiveSmallIntegerField(blank=True, null=True)
 
     def __str__(self):
         return "%s" % (self.title)
+
+class Media(models.Model):
+    type = models.CharField(choices=MEDIA_CHOICES, max_length=200)
+    # for audio, video and other: URL
+    # for image: local folder path
+    url = models.CharField(max_length=200)
+
+    def __str__(self):
+        return "%s" % (self.url)
 
 class Artist(models.Model):
     first_name = models.CharField(max_length=200)
